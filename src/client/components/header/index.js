1386068626c14a20d7ee750e19ed7ba64fb72078
Dmitry Home <di@goldcarrot.ru>
2018-03-11 17:49:18 +0500

    add hamburger

--- src/client/components/header/index.js
+++ src/client/components/header/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import './header.scss'
+import '../../assets/styles/hamburgers.scss'
 export default class Header extends Component {
     constructor(props){
         super(props);
@@ -9,10 +10,13 @@ export default class Header extends Component {
         return (
             <header className="header">
                 <div className="header-hamburger">
-                    <button className="header-hamburger__button"></button>
+                    <button class="hamburger hamburger--minus is-active" type="button">
+                        <span class="hamburger-box">
+                            <span class="hamburger-inner"></span>
+                        </span>
+                    </button>
                     <div className="header-hamburger__text">Welcome</div>
                 </div>
-                
             </header>
         )
     }
--- src/client/containers/home/index.js
+++ src/client/containers/home/index.js
@@ -5,6 +5,7 @@ import videoMP4 from '../../assets/video/video-background.mp4'
 import InputMask from 'react-input-mask';
 import serialize from 'form-serialize';
 import './home-page.scss'
+// import AboutSection from './about-section'
 
 export default class Home extends Component {
     constructor(props){
@@ -18,60 +19,63 @@ export default class Home extends Component {
     submitCallback(e){
         e.preventDefault();
         let form_data = serialize(e.target, {hash:true})
-        console.log(serialize(e.target, { hash: true }))
+        console.log('callback form data: ', form_data)
     }
     componentDidMount(){    
     }
     render() {
         return (
-            <section class="main-video">
-                <div className="video-wrap">
-                    <video id="home-page-video" class="video-src" loop width="100%" height="100%"  autoPlay={true}>
-                        <source src={videoWebm} type="video/webm; codecs='vp8, vorbis'"/>
-                        <source src={videoMP4} type="video/mp4"/>
-                    </video>
-                    <div className="content">
-                        <h1>Digital агентство %NAME%</h1>
-                        <h2>
-                            Разработка и комплексное продвижение сайтов.<br/>
-                            Создание мобильных приложений
-                        </h2>
-                        <div className="services">
-                            <div className="services__title">Мы предлагаем:</div>
-                            <div className="services-blocks">
-                                <Link to="/home" className="services__link">Разработка веб-сайтов</Link>
-                                <Link to="/home" className="services__link">Продвижение и реклама в интернете</Link>
-                                <Link to="/home" className="services__link">Разаботка мобильных приложений</Link>
-                                <Link to="/home" className="services__link">Ведение вашего бренда в соц сетях</Link>
+            <div>
+                <section class="main-video">
+                    <div className="video-wrap">
+                        <video id="home-page-video" class="video-src" loop width="100%" height="100%"  autoPlay={true}>
+                            <source src={videoWebm} type="video/webm; codecs='vp8, vorbis'"/>
+                            <source src={videoMP4} type="video/mp4"/>
+                        </video>
+                        <div className="content">
+                            <h1>Digital агентство %NAME%</h1>
+                            <h2>
+                                Разработка и комплексное продвижение сайтов.<br/>
+                                Создание мобильных приложений
+                            </h2>
+                            <div className="services">
+                                <div className="services__title">Мы предлагаем:</div>
+                                <div className="services-blocks">
+                                    <Link to="/home" className="services__link">Разработка веб-сайтов</Link>
+                                    <Link to="/home" className="services__link">Продвижение и реклама в интернете</Link>
+                                    <Link to="/home" className="services__link">Разаботка мобильных приложений</Link>
+                                    <Link to="/home" className="services__link">Ведение вашего бренда в соц сетях</Link>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="callback-form">
+                            <div className="content">
+                                <div className="callback-form__title">Бесплатная консультация</div>
+                                <form id="#example-form" onSubmit={this.submitCallback.bind(this)}>
+                                    <InputMask 
+                                        name="name"
+                                        ref={input => { this.firstNameInp = input; }}
+                                        type="text"
+                                        placeholder="Ваше имя" 
+                                        mask="aaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+                                        maskChar=""
+                                        formatChars={{a: '[A-Za-zА-Яа-я]'}}
+                                    />
+                                    <InputMask 
+                                        name="phone" 
+                                        ref={input => { this.phoneInp = input; }}
+                                        type="text"
+                                        placeholder="Телефон"
+                                        mask="+7(999) 999-99-99"
+                                    />
+                                    <button>Жду звонка</button>
+                                </form>
                             </div>
                         </div>
                     </div>
-                    <div className="callback-form">
-                        <div className="content">
-                            <div className="callback-form__title">Бесплатная консультация</div>
-                            <form id="#example-form" onSubmit={this.submitCallback.bind(this)}>
-                                <InputMask 
-                                    name="name"
-                                    ref={input => { this.firstNameInp = input; }}
-                                    type="text"
-                                    placeholder="Ваше имя" 
-                                    mask="aaaaaaaaaaaaaaaaaaaaaaaaaaaa"
-                                    maskChar=""
-                                    formatChars={{a: '[A-Za-zА-Яа-я]'}}
-                                />
-                                <InputMask 
-                                    name="phone" 
-                                    ref={input => { this.phoneInp = input; }}
-                                    type="text"
-                                    placeholder="Телефон"
-                                    mask="+7(999) 999-99-99"
-                                />
-                                <button>Жду звонка</button>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </section>
+                </section>
+                {/* <AboutSection /> */}
+            </div>
         )
     }
 }
