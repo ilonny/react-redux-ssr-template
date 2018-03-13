dec39344c136b864e36b09ddbbf99a85e4be9c4c
Dmitry Home <di@goldcarrot.ru>
2018-03-13 20:04:19 +0500

    q

---
+++ src/client/components/sidebar/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import './sidebar.scss'
+export default class Sidebar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        }
+    }
+    render() {
+        return (
+            <aside>
+            </aside>
+        )
+    }
+}
--- src/client/containers/AppRoot.js
+++ src/client/containers/AppRoot.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 // import Link from 'react-router-dom/Link';
 import { renderRoutes } from 'react-router-config';
 import Header from '../components/header/index'
+import Sidebar from '../components/sidebar/index'
 
 export default class AppRoot extends Component {
     render() {
         return (
             <div id="container">
-                <Header/>
+                <Header />
+                <Sidebar />
                 <main className="mdl-layout__content">
                     {renderRoutes(this.props.route.routes)}
                 </main>
